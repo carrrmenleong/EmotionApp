4721ef9caceda7d4b05b119538953517a537fdf7
Carmen <carmen020824@gmail.com>
2022-08-20 16:34:16 +0800

    set up dev env, db, signup login feature

---
+++ app/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+class SignupForm(FlaskForm):
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    orcid = StringField('ORCID', validators=[DataRequired()])
+    institution = StringField('Institution', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    email2 = StringField('Confirm Email', validators=[DataRequired(), Email(), EqualTo('email')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    reason = TextAreaField('Reason for using this app (minimum 100 words)', validators=[DataRequired(), Length(min=100)])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
