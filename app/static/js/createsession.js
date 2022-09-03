99017aaa49e5835661583ba3a0c6aa92526996de
MandealTan <22647372@student.uwa.edu.au>
2022-09-03 11:43:33 +0800

    Submitting Creation Session 02

--- app/static/js/createsession.js
+++ app/static/js/createsession.js
@@ -1,22 +1,35 @@
-var counter = 1
+var counter = 2
 
 //adding question block
-function add(){
+function add(target){
     counter ++
     var close
 
     //retrieving parent div container
-    parent = document.getElementById('questions')
+    if (target == 'pre'){
+        parent = document.getElementById('pre_questions')
+    }
+    else{
+        parent = document.getElementById('post_questions')
+    }
     
     //creating necessary elements
     question = document.createElement('textarea')
     close = document.createElement('span')
 
     //setting attributes of elements
-    question.setAttribute('class', 'question')
+    if (target == 'pre'){
+        question.setAttribute('class', 'preQ')
+        close.setAttribute('id', 'preqD'+counter)
+    }
+    else{
+        question.setAttribute('class', 'postQ')
+        close.setAttribute('id', 'postD'+counter)
+    }
+    //question.setAttribute('class', 'question')
     close.textContent = 'X'
     close.setAttribute('class', 'del')
-    close.setAttribute('id', 'd'+counter)
+    //close.setAttribute('id', 'd'+counter)
     close.setAttribute('onclick', 'del(this)')
 
     //creating child to be appended into parent
@@ -33,10 +46,17 @@ function add(){
 // delete question block
 function del(x){
     target = x.id.slice(-1)
-
     child = document.getElementById(target)
-    parent = document.getElementById('questions')
-
+    
+    //if target belongs to prequestions
+    if(x.id.includes('pre')){
+        parent = document.getElementById('pre_questions')
+    }
+    //if target belongs to postquestions
+    else{
+        parent = document.getElementById('post_questions')
+    }
+    
     parent.removeChild(child)
 }
 
@@ -44,29 +64,84 @@ function del(x){
 function submit(){
 
     var prequestions = []
+    var postquestions = []
 
     // retrieving title
-    title = document.getElementById('title').value
+    var title = document.getElementById('title').value
 
     // retrieving consent
-    consent = document.getElementById('consent').value
+    var consent = document.getElementById('consent').value
 
     // retrieving all prequestions
-    all = document.getElementsByClassName('question')
-    for(let i = 0; i< all.length; i ++){
-        //if question is mcq
-        if(all[i].value.includes('\n')){
-            temp = all[i].value.split('\n')
-            question = temp[0]
-            temp.shift()
-            option = temp.join('\n')
-            final = question + '\n' + option
-        }
-        //if question is open ended
-        else{
-            final = all[i].value
-        }
-        prequestions.push(final)
+    var all = document.getElementsByClassName('preQ')
+    //if there are no prequestions
+    if (all == null){
+        prequestions = []
     }
+    else{
+        for(let i = 0; i< all.length; i ++){
+            //if question is mcq
+            if(all[i].value.includes('\n')){
+                var temp = all[i].value.split('\n')
+                var question = temp[0]
+                temp.shift()
+                option = temp.join('\n')
+                final = question + '\n' + option
+            }
+            //if question is open ended
+            else{
+                final = all[i].value
+            }
+            prequestions.push(final)
+        }
+    }
+    console.log(typeof prequestions)
+
+
+    // retrieving all emotions
+    emotion = document.getElementById('emotion').value
+
+    // retrieving all intensity
+    intensity = parseInt(document.getElementById('intensity').value)
+    console.log(Number.isInteger(intensity))
+
+    // retrieving all post questions
+    all = document.getElementsByClassName('postQ')
+    for(let i = 0; i< all.length; i ++){
+        postquestions.push(all[i].value)
+    }
+    console.log(postquestions)
+
+    //combining the data
+    mydata =  {
+        sessionTitle:title, 
+        consent: consent, 
+        preQuestions: prequestions, 
+        emotions: emotion, 
+        intensity:intensity, 
+        postQuestions:postquestions
+    }
+    console.log("DATA:",mydata)
+
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+
+        if (xhttp.readyState === XMLHttpRequest.DONE) {
+            const status = xhttp.status;
+            if (status === 0 || (status >= 200 && status < 400)) {
+                // The request has been completed successfully
+                console.log('Submitted!')
+                console.log(xhttp.responseText);
+            } else {
+                console.log(xhttp.responseText);
+                // Oh no! There has been an error with the request!
+            }
+        }
+    }
+    xhttp.open('POST', '/createsession', true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify(mydata));
 
 }
+
