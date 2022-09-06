f2ebf40773e3efd2e6fc269452f035640d4d150b
waiyeewwy <22769803@student.uwa.edu.au>
2022-09-06 19:15:52 +0800

    send password reset link

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
-from app import db, login
+from app import db, login, app
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from time import time
+import jwt
 
 
 # Each row in this table represent an Admin(Researcher) or a Superadmin
@@ -31,6 +33,20 @@ class User(UserMixin, db.Model):
     @login.user_loader
     def load_user(id):
         return User.query.get(int(id))
+    
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 
 # Each row in this table represent a survey session
