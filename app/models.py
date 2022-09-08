75473f0edd904c0592a1d204f8fbc04088ae44b3
Carmen <carmen020824@gmail.com>
2022-09-08 16:35:29 +0800

    fix receiving email issue, and jwt decode issue

--- app/models.py
+++ app/models.py
@@ -36,14 +36,12 @@ class User(UserMixin, db.Model):
     
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
-            {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256')
+            {'reset_password': self.id, 'exp': time() + expires_in}, app.config['SECRET_KEY'], algorithm='HS256')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
-                            algorithms=['HS256'])['reset_password']
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
