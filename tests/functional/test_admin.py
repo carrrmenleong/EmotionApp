5455c1089677a094b617ddff8fdc842a35f52651
Carmen <carmen020824@gmail.com>
2022-10-13 14:50:15 +0800

    merge main, fix login_superadmin error

--- tests/functional/test_admin.py
+++ tests/functional/test_admin.py
@@ -4,7 +4,7 @@ def test_login_already_logged_in(test_client, init_database, login_superadmin):
     WHEN the '/login' page is requested (GET) when the superadmin is already logged in
     THEN check superadmin is redirected to viewsession or viewusers page
     """
-    response = test_client.get('/auth/login',follow_redirects=True)
+    response = test_client.get('/login',follow_redirects=True)
     assert response.status_code == 200
     assert b'View Sessions' in response.data
     assert b'Logout' in response.data
@@ -14,7 +14,7 @@ def test_login_already_logged_in(test_client, init_database, login_superadmin):
     WHEN the '/logout' page is requested (GET)
     THEN check the response is valid
     """
-    response = test_client.get('/auth/logout', follow_redirects=True)
+    response = test_client.get('/logout', follow_redirects=True)
     assert response.status_code == 200
     assert b'Login' in response.data
     assert b'Email' in response.data
