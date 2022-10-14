44a5d1080de27a1aaa63412383226f31e89f6f5c
waiyeewwy <22769803@student.uwa.edu.au>
2022-10-14 14:07:41 +0800

    admin testing with failure
    - error 400 or 404

--- tests/functional/test_admin.py
+++ tests/functional/test_admin.py
@@ -1,4 +1,7 @@
-def test_login_already_logged_in(test_client, init_database, login_superadmin):
+from app.models import Session
+import json
+
+def test_logged_in_as_superadmin(test_client, init_database, login_superadmin):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/login' page is requested (GET) when the superadmin is already logged in
@@ -6,7 +9,37 @@ def test_login_already_logged_in(test_client, init_database, login_superadmin):
     """
     response = test_client.get('/login',follow_redirects=True)
     assert response.status_code == 200
+    assert b'View Users' in response.data
+    assert b'Logout' in response.data
+
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/superadmin_viewsessions' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/viewsessions', follow_redirects=True)
+    assert response.status_code == 200
     assert b'View Sessions' in response.data
+    assert b'Logout' in response.data
+
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/viewsession/<int:sessionid>' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/viewsession/2')
+    assert response.status_code == 200
+    assert b'View Sessions' in response.data
+    assert b'Logout' in response.data
+
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/signupreq' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/signupreq')
+    assert response.status_code == 200
+    assert b'Sign Up Requests' in response.data
     assert b'Logout' in response.data
 
     """ 
@@ -22,3 +55,144 @@ def test_login_already_logged_in(test_client, init_database, login_superadmin):
 
 
 
+
+def test_logged_in_as_admin(test_client,init_database,login_default_user):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/viewsessions' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/viewsessions')
+    assert response.status_code == 200
+    assert b'View Sessions' in response.data
+    assert b'Logout' in response.data
+
+
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/viewsession/<int:sessionid>' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/viewsession/1')
+    assert response.status_code == 200
+    assert b'View Sessions' in response.data
+    assert b'Logout' in response.data
+
+
+def test_approve_user(test_client, init_database, login_superadmin):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/approveUser' page is posted (POST)
+    WHEN superadmin click the 'Approve' button
+    THEN check the response is valid
+    """
+    response = test_client.post('/approveUser', json="4", follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Sign Up Requests' in response.data
+
+
+def test_deny_user(test_client, init_database, login_superadmin):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/denyUser' page is posted (POST)
+    WHEN superadmin click the 'Deny' button
+    THEN check the response is valid
+    """
+    response = test_client.post('/denyUser', json="4", follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Sign Up Requests' in response.data
+
+
+def test_delete_user(test_client, init_database, login_superadmin):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/deleteUser' page is posted (POST)
+    WHEN superadmin click the 'Delete' button
+    THEN check the response is valid
+    """
+    response = test_client.post('/deleteUser', json="1", follow_redirects=True)
+    assert response.status_code == 200
+    assert b'success' in response.data
+
+
+def test_modify_session(test_client, init_database, login_default_user):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/copySession/<int:id>' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/copySession/1')
+    assert response.status_code == 200
+    assert b'Create Session' in response.data
+    
+
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/editSession/<int:id>' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/editSession/1')
+    assert response.status_code == 200
+    assert b'Edit Session' in response.data
+
+
+def test_publish_session(test_client, init_database, login_default_user):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/publishSession' page is posted (POST)
+    THEN check the response is valid
+    """
+    response = test_client.post('/publishSession',
+                                json="3",
+                                follow_redirects=True)
+    assert response.status_code == 200
+    assert b'View Session' in response.data
+    
+
+def test_update_session(test_client, init_database, login_default_user):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/updateSession' page is posted (POST)
+    THEN check the response is valid
+    """
+    response = test_client.post('/updateSession',
+                                data=json.dumps({'id':1,
+                                    'sessionTitle':'session1a',
+                                    'consent': True,
+                                    'emotions':'Anxious',
+                                    'intensity':'10',
+                                    'preQuestions': 'preQues1',
+                                    'postQuestions': 'postQues1'
+                                    }),
+                                follow_redirects = True
+                                )
+    assert response.status_code == 200
+    assert b'View Session' in response.data
+
+
+def test_delete_session(test_client, init_database, login_superadmin):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/deleteSession' page is posted (POST)
+    THEN check the response is valid
+    """
+    response = test_client.post('/deleteSession',
+                                json="1",
+                                follow_redirects = True
+                                )
+    assert response.status_code == 200
+    assert b'success' in response.data
+
+
+def test_delete_result(test_client, init_database, login_superadmin):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/deleteresult' page is posted (POST)
+    THEN check the response is valid
+    """
+    response = test_client.post('/deleteresult',
+                                data=json.dumps({'sessionid':1,'participantid':1}),
+                                follow_redirects = True
+                                )
+    assert response.status_code == 200
+    assert b'success' in response.data
