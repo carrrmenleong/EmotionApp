828546b8790d11985c53b09a2b2b2a14d4dadf1a
Carmen <carmen020824@gmail.com>
2023-03-01 22:47:02 +0800

    hardcode config to debug

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_USERNAME = "emotionappdev2022@gmail.com"
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = [os.environ.get('MAIL_USERNAME')]
+    ADMINS = ["emotionappdev2022@gmail.com"]
 
 
 class TestingConfig(Config):
