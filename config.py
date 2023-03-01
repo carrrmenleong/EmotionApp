9670699e97380a08297fd0600afc7b0051f36c14
Carmen <carmen020824@gmail.com>
2023-03-01 23:08:15 +0800

    debug

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = [os.environ.get('MAIL_USERNAME')]
+    ADMINS = [os.environ['MAIL_USERNAME']]
 
 
 class TestingConfig(Config):
