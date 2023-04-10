44ee331d0547779311d28a05f2be121ddac4814d
Carmen <carmen020824@gmail.com>
2023-04-10 11:56:49 +0800

    remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
-MAIL_USERNAME = os.environ['MAIL_USERNAME']
-print(MAIL_USERNAME)
-print(os.getcwd())
